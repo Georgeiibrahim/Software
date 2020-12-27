8cf6a86d25ff470d09c78a2c602bc122b182ab94
malkrizk <64382140+malkrizk@users.noreply.github.com>
2020-12-27 02:23:12 +0200

    Add files via upload

---
+++ demorest2/src/main/java/com/telusko/demorest2/Language.java
@@ -0,0 +1,14 @@
+package com.telusko.demorest2;
+
+public enum Language {
+	Arabic,English;
+	public String toString() {
+		switch(this)
+		{
+		case Arabic:return "Arabic";
+		case English: return "English";
+		default:
+			return "UNSPECIFIED";
+		}
+	};
+}
---
+++ demorest2/src/main/java/com/telusko/demorest2/NotificationTemplate.java
@@ -0,0 +1,56 @@
+package com.telusko.demorest2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
+public class NotificationTemplate {
+	int id;
+	static int counter=0;
+	private String subject;
+	private String body;
+	int language;	
+	public NotificationTemplate() {
+		// TODO Auto-generated constructor stub
+	}
+	public NotificationTemplate(String subject,String body,int L)
+	{
+		id=++counter;
+		this.subject=subject;
+		this.body=body;
+		language=L;
+		
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getBody() {
+		return body;
+	}
+
+	public void setLanguage(int language) {
+		this.language = language;
+	}
+	public int getLanguage() {
+		return language;
+	}
+	
+	
+	public  int getId() {
+		return id;
+	}
+	public  void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
